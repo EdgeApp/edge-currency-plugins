0668bdff37d9462f15113ce6e4ef6c735bdc3fb3
TheCharlatan <seb.kung@gmail.com>
2020-06-17 23:16:01 +0200

    Basic implementation of typescript helper functions for bitcoinjs-lib
    This commit implements some basic interfaces to be used in the future by
    the edge wallet. They include bip32 key generation, transforms between
    different key formats and transaction generation.
    
    A lot of function signatures and interfaces currently contain spurious
    definitions that lead to potentially undefined data. This is by
    intention, since there is no error handling yet. Error definitions still
    need to be defined properly.
    
    The commit also does not implement multicoin support yet. Again, this
    needs some further consideration before implementation.
    
    Functionality wise the main TODOs are implementing more transaction
    tests, e.g. multi input, segwit transaction, transactions with all
    output types, groestl coin sighashes, cashaddr support and relay
    protection tx support.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,367 @@
-import CONFIG from '../config.json'
+import * as bip32 from 'bip32'
+import { BIP32Interface } from 'bip32'
+import * as bip39 from 'bip39'
+import * as bitcoin from 'bitcoinjs-lib'
 
-function foo(): void {
-  console.log('Hello, world')
-  console.log('apiKey:', CONFIG.apiKey)
+export enum NetworkEnum {
+  Mainnet,
+  Testnet
 }
 
-foo()
+// bip32, bip49, bip84 xpub prefixes
+export enum BIP43PurposeTypeEnum {
+  Legacy, // xpub/xprv tpub/tprv
+  Segwit, // zpub/zprv vpub/vprv
+  WrappedSegwit // ypub/yprv upub/uprv
+}
+
+// supported address types. Notice that p2wsh is not included.
+export enum AddressTypeEnum {
+  p2pkh,
+  p2sh,
+  p2wpkhp2sh,
+  p2wpkh,
+  p2wsh, // TODO: both witness script hash variants have not been implemented so far.
+  p2wshp2sh
+}
+
+export enum TransactionInputTypeEnum {
+  Legacy,
+  Segwit
+}
+
+export interface MnemonicToXPrivArgs {
+  mnemonic: string
+  path: string
+  network: NetworkEnum
+  type: BIP43PurposeTypeEnum
+}
+
+export interface XPrivToXPubArgs {
+  xpriv: string
+  network: NetworkEnum
+  type: BIP43PurposeTypeEnum
+}
+
+export interface XPrivToPrivateKeyArgs {
+  xpriv: string
+  network: NetworkEnum
+  type: BIP43PurposeTypeEnum
+  bip44ChangeIndex: 0 | 1
+  bip44AddressIndex: number
+}
+
+// Careful! Calling this the ScriptHash is only correct for p2sh addresses.
+// For p2pkh and p2wpkh this is just the pubkey hash.
+export interface XPubToScriptHashArgs {
+  xpub: string
+  network: NetworkEnum
+  type: BIP43PurposeTypeEnum
+  addressType: AddressTypeEnum
+  bip44ChangeIndex: 0 | 1
+  bip44AddressIndex: number
+}
+
+export interface ScriptHashToAddressArgs {
+  scriptHash: Buffer | undefined
+  network: NetworkEnum
+  addressType: AddressTypeEnum
+}
+
+export interface AddressToScriptHashArgs {
+  address: string
+  network: NetworkEnum
+  addressType: AddressTypeEnum
+}
+
+export interface AddressToScriptPubkeyArgs {
+  address: string
+  network: NetworkEnum
+  addressType: AddressTypeEnum
+}
+
+export interface PubkeyToScriptPubkeyArgs {
+  pubkey: Buffer
+  addressType: AddressTypeEnum
+}
+
+export interface WIFToECPairArgs {
+  wifKey: string
+  network?: NetworkEnum
+}
+
+export type ECPair = bitcoin.ECPairInterface
+
+export interface TxInput {
+  type: TransactionInputTypeEnum
+  prev_txid: string
+  index: number
+  prev_txout: Buffer // relevant for legacy transactions
+  prev_scriptPubkey: Buffer // relevant for segwit transactions, maybe make it optional in the future
+  sequence: number
+}
+
+export interface TxOutput {
+  scriptPubkey: Buffer
+  amount: number
+}
+
+export interface CreateTxArgs {
+  network: NetworkEnum
+  inputs: TxInput[]
+  outputs: TxOutput[]
+  locktime: number
+  privateKey: ECPair
+}
+
+interface BIP32Network {
+  wif: number
+  bip32: {
+    public: number
+    private: number
+  }
+  messagePrefix?: string
+  bech32?: string
+  pubKeyHash?: number
+  scriptHash?: number
+}
+
+// helper function for the extended key prefix constant pairs
+function getBIP32NetworkPrefixBytes(
+  networkType: NetworkEnum,
+  sigType: BIP43PurposeTypeEnum
+): BIP32Network {
+  switch (networkType) {
+    case NetworkEnum.Testnet:
+      switch (sigType) {
+        case BIP43PurposeTypeEnum.Segwit:
+          return {
+            wif: 0xef,
+            bip32: { public: 0x045f1cf6, private: 0x045f18bc }
+          }
+        case BIP43PurposeTypeEnum.WrappedSegwit:
+          return {
+            wif: 0xef,
+            bip32: { public: 0x044a5262, private: 0x044a4e28 }
+          }
+        // always assume the basic "legacy" xpub type
+        default:
+          return {
+            wif: 0xef,
+            bip32: { public: 0x043587cf, private: 0x04358394 }
+          }
+      }
+    // always assume mainnet
+    default:
+      switch (sigType) {
+        case BIP43PurposeTypeEnum.Segwit:
+          return {
+            wif: 0x80,
+            bip32: { public: 0x04b24746, private: 0x04b2430c }
+          }
+        case BIP43PurposeTypeEnum.WrappedSegwit:
+          return {
+            wif: 0x80,
+            bip32: { public: 0x049d7cb2, private: 0x049d7878 }
+          }
+        // always assume the basic "legacy" xpub type
+        default:
+          return {
+            wif: 0x80,
+            bip32: { public: 0x0488b21e, private: 0x0488ade4 }
+          }
+      }
+  }
+}
+
+export function mnemonicToXPriv(
+  mnemonicToXPrivArgs: MnemonicToXPrivArgs
+): string {
+  const seed = bip39.mnemonicToSeedSync(mnemonicToXPrivArgs.mnemonic)
+  const root: BIP32Interface = bip32.fromSeed(seed)
+  const network: BIP32Network = getBIP32NetworkPrefixBytes(
+    mnemonicToXPrivArgs.network,
+    mnemonicToXPrivArgs.type
+  )
+  root.network = network
+  const child: BIP32Interface = root.derivePath(mnemonicToXPrivArgs.path)
+  return child.toBase58()
+}
+
+export function xprivToXPub(xprivToXPubArgs: XPrivToXPubArgs): string {
+  const network: BIP32Network = getBIP32NetworkPrefixBytes(
+    xprivToXPubArgs.network,
+    xprivToXPubArgs.type
+  )
+  const node: BIP32Interface = bip32.fromBase58(xprivToXPubArgs.xpriv, network)
+  return node.neutered().toBase58()
+}
+
+// return pubkey hash / script hash based on chosen type and network
+export function xpubToScriptHash(
+  xpubToScriptHashArgs: XPubToScriptHashArgs
+): Buffer | undefined {
+  const network: BIP32Network = getBIP32NetworkPrefixBytes(
+    xpubToScriptHashArgs.network,
+    xpubToScriptHashArgs.type
+  )
+  const node: BIP32Interface = bip32.fromBase58(
+    xpubToScriptHashArgs.xpub,
+    network
+  )
+  const pubkey: Buffer = node
+    .derive(xpubToScriptHashArgs.bip44ChangeIndex)
+    .derive(xpubToScriptHashArgs.bip44AddressIndex).publicKey
+  switch (xpubToScriptHashArgs.addressType) {
+    case AddressTypeEnum.p2pkh:
+      return bitcoin.payments.p2pkh({ pubkey }).hash
+    case AddressTypeEnum.p2sh:
+      return bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2pkh({ pubkey })
+      }).hash
+    case AddressTypeEnum.p2wpkhp2sh:
+      return bitcoin.payments.p2sh({
+        redeem: bitcoin.payments.p2wpkh({ pubkey })
+      }).hash
+    case AddressTypeEnum.p2wpkh:
+      return bitcoin.payments.p2wpkh({ pubkey }).hash
+  }
+}
+
+// take passed in script hash (for p2sh)/ pubkey hash (for p2pkh and p2wpkh) and encode as address of choice
+export function scriptHashToAddress(
+  scriptHashToAddressArgs: ScriptHashToAddressArgs
+): string | undefined {
+  switch (scriptHashToAddressArgs.addressType) {
+    case AddressTypeEnum.p2pkh:
+      return bitcoin.payments.p2pkh({
+        hash: scriptHashToAddressArgs.scriptHash
+      }).address
+    case AddressTypeEnum.p2sh:
+    case AddressTypeEnum.p2wpkhp2sh:
+      return bitcoin.payments.p2sh({ hash: scriptHashToAddressArgs.scriptHash })
+        .address
+    case AddressTypeEnum.p2wpkh:
+      return bitcoin.payments.p2wpkh({
+        hash: scriptHashToAddressArgs.scriptHash
+      }).address
+  }
+}
+
+// take an address and return either a script hash (for a p2sh address) or a pubkey hash (for p2pkh and p2wpkh)
+export function addressToScriptHash(
+  addressToScriptHashArgs: AddressToScriptHashArgs
+): Buffer | undefined {
+  switch (addressToScriptHashArgs.addressType) {
+    case AddressTypeEnum.p2pkh:
+      return bitcoin.payments.p2pkh({
+        address: addressToScriptHashArgs.address
+      }).hash
+    case AddressTypeEnum.p2sh:
+    case AddressTypeEnum.p2wpkhp2sh:
+      return bitcoin.payments.p2sh({ address: addressToScriptHashArgs.address })
+        .hash
+    case AddressTypeEnum.p2wpkh:
+      return bitcoin.payments.p2wpkh({
+        address: addressToScriptHashArgs.address
+      }).hash
+  }
+}
+
+export function addressToScriptPubkey(
+  addressToScriptPubkeyArgs: AddressToScriptPubkeyArgs
+): Buffer | undefined {
+  switch (addressToScriptPubkeyArgs.addressType) {
+    case AddressTypeEnum.p2pkh:
+      return bitcoin.payments.p2pkh({
+        address: addressToScriptPubkeyArgs.address
+      }).output
+    case AddressTypeEnum.p2sh:
+    case AddressTypeEnum.p2wpkhp2sh:
+      return bitcoin.payments.p2sh({
+        address: addressToScriptPubkeyArgs.address
+      }).output
+    case AddressTypeEnum.p2wpkh:
+      return bitcoin.payments.p2wpkh({
+        address: addressToScriptPubkeyArgs.address
+      }).output
+  }
+}
+
+export function pubkeyToScriptPubkey(
+  pubkeyToScriptPubkeyArgs: PubkeyToScriptPubkeyArgs
+): Buffer | undefined {
+  switch (pubkeyToScriptPubkeyArgs.addressType) {
+    case AddressTypeEnum.p2pkh:
+      return bitcoin.payments.p2pkh({ pubkey: pubkeyToScriptPubkeyArgs.pubkey })
+        .output
+    case AddressTypeEnum.p2sh:
+    case AddressTypeEnum.p2wpkhp2sh:
+      return bitcoin.payments.p2sh({ pubkey: pubkeyToScriptPubkeyArgs.pubkey })
+        .output
+    case AddressTypeEnum.p2wpkh:
+      return bitcoin.payments.p2wpkh({
+        pubkey: pubkeyToScriptPubkeyArgs.pubkey
+      }).output
+  }
+}
+
+export function xprivToPrivateKey(
+  xprivToPrivateKeyArgs: XPrivToPrivateKeyArgs
+): Buffer | undefined {
+  const network: BIP32Network = getBIP32NetworkPrefixBytes(
+    xprivToPrivateKeyArgs.network,
+    xprivToPrivateKeyArgs.type
+  )
+  const node: BIP32Interface = bip32.fromBase58(
+    xprivToPrivateKeyArgs.xpriv,
+    network
+  )
+  return node.derive(0).derive(0).privateKey
+}
+
+export function wifToECPair(wifToECPairArgs: WIFToECPairArgs): ECPair {
+  // later we can pass in an extra network here
+  return bitcoin.ECPair.fromWIF(
+    'L2uPYXe17xSTqbCjZvL2DsyXPCbXspvcu5mHLDYUgzdUbZGSKrSr'
+  )
+}
+
+export function createTx(createTxArgs: CreateTxArgs): string {
+  const alice = bitcoin.ECPair.fromWIF(
+    'L2uPYXe17xSTqbCjZvL2DsyXPCbXspvcu5mHLDYUgzdUbZGSKrSr'
+  )
+  const psbt = new bitcoin.Psbt()
+  for (let i: number = 0; i < createTxArgs.inputs.length; i++) {
+    if (createTxArgs.inputs[i].type === TransactionInputTypeEnum.Legacy) {
+      psbt.addInput({
+        hash: createTxArgs.inputs[i].prev_txid,
+        index: 0,
+        sequence: createTxArgs.inputs[i].sequence,
+        // non-segwit inputs now require passing the whole previous tx as Buffer
+        nonWitnessUtxo: createTxArgs.inputs[i].prev_txout
+      })
+    } else {
+      psbt.addInput({
+        hash: createTxArgs.inputs[i].prev_txid,
+        index: 0,
+        sequence: createTxArgs.inputs[i].sequence,
+        // add witnessUtxo for Segwit input type. The scriptPubkey and the value only are needed.
+        witnessUtxo: {
+          script: createTxArgs.inputs[i].prev_scriptPubkey,
+          value: 90000
+        }
+      })
+    }
+  }
+  for (let i: number = 0; i < createTxArgs.outputs.length; i++) {
+    psbt.addOutput({
+      script: createTxArgs.outputs[i].scriptPubkey,
+      value: 80000
+    })
+  }
+  psbt.signInput(0, alice)
+  psbt.validateSignaturesOfInput(0)
+  psbt.finalizeAllInputs()
+  return psbt.extractTransaction().toHex()
+}
--- test/dummy-test.ts
+++ test/dummy-test.ts
@@ -1,6 +1,323 @@
 import { expect } from 'chai'
 import { describe, it } from 'mocha'
 
+import {
+  addressToScriptHash,
+  addressToScriptPubkey,
+  AddressTypeEnum,
+  BIP43PurposeTypeEnum,
+  createTx,
+  mnemonicToXPriv,
+  NetworkEnum,
+  pubkeyToScriptPubkey,
+  scriptHashToAddress,
+  TransactionInputTypeEnum,
+  wifToECPair,
+  xprivToXPub,
+  xpubToScriptHash
+} from '../src/index'
+
+describe('mnemonic to xprv tests', () => {
+  it('bip32 test vectors as collected from BIP84, BIP49 and some generated cases to test xpub prefix bytes', () => {
+    const mnemonic =
+      'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
+    const resultSegwit = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/84'/0'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Segwit
+    })
+    expect(resultSegwit).to.equal(
+      'zprvAdG4iTXWBoARxkkzNpNh8r6Qag3irQB8PzEMkAFeTRXxHpbF9z4QgEvBRmfvqWvGp42t42nvgGpNgYSJA9iefm1yYNZKEm7z6qUWCroSQnE'
+    )
+
+    const resultSegwitTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/84'/1'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Segwit
+    })
+    expect(resultSegwitTestnet).to.equal(
+      'vprv9K7GLAaERuM58PVvbk1sMo7wzVCoPwzZpVXLRBmum93gL5pSqQCAAvZjtmz93nnnYMr9i2FwG2fqrwYLRgJmDDwFjGiamGsbRMJ5Y6siJ8H'
+    )
+
+    const resultWrappedSegwit = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/49'/0'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit
+    })
+    expect(resultWrappedSegwit).to.equal(
+      'yprvAHwhK6RbpuS3dgCYHM5jc2ZvEKd7Bi61u9FVhYMpgMSuZS613T1xxQeKTffhrHY79hZ5PsskBjcc6C2V7DrnsMsNaGDaWev3GLRQRgV7hxF'
+    )
+
+    const resultWrappedSegwitTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/49'/1'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit
+    })
+    expect(resultWrappedSegwitTestnet).to.equal(
+      'uprv91G7gZkzehuMVxDJTYE6tLivdF8e4rvzSu1LFfKw3b2Qx1Aj8vpoFnHdfUZ3hmi9jsvPifmZ24RTN2KhwB8BfMLTVqaBReibyaFFcTP1s9n'
+    )
+
+    const resultLegacy = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/44'/0'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Legacy
+    })
+    expect(resultLegacy).to.equal(
+      'xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb'
+    )
+
+    const resultLegacyTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/44'/0'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Legacy
+    })
+    expect(resultLegacyTestnet).to.equal(
+      'tprv8fVU32aAEuEPeH1WYx3LhXtSFZTRaFqjbFNPaJZ9R8fCVja44tSaUPZEKGpMK6McUDkWWMvRiVfKR3Wzei6AmLoTNYHMAZ9KtvVTLZZdhvA'
+    )
+  })
+})
+
+describe('xpriv to xpub tests', () => {
+  it('bip32 prefix tests for the conversion from xpriv to xpub', () => {
+    const resultSegwit = xprivToXPub({
+      xpriv:
+        'zprvAdG4iTXWBoARxkkzNpNh8r6Qag3irQB8PzEMkAFeTRXxHpbF9z4QgEvBRmfvqWvGp42t42nvgGpNgYSJA9iefm1yYNZKEm7z6qUWCroSQnE',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Segwit
+    })
+    expect(resultSegwit).to.equals(
+      'zpub6rFR7y4Q2AijBEqTUquhVz398htDFrtymD9xYYfG1m4wAcvPhXNfE3EfH1r1ADqtfSdVCToUG868RvUUkgDKf31mGDtKsAYz2oz2AGutZYs'
+    )
+
+    const resultSegwitTestnet = xprivToXPub({
+      xpriv:
+        'vprv9K7GLAaERuM58PVvbk1sMo7wzVCoPwzZpVXLRBmum93gL5pSqQCAAvZjtmz93nnnYMr9i2FwG2fqrwYLRgJmDDwFjGiamGsbRMJ5Y6siJ8H',
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Segwit
+    })
+    expect(resultSegwitTestnet).to.equals(
+      'vpub5Y6cjg78GGuNLsaPhmYsiw4gYX3HoQiRBiSwDaBXKUafCt9bNwWQiitDk5VZ5BVxYnQdwoTyXSs2JHRPAgjAvtbBrf8ZhDYe2jWAqvZVnsc'
+    )
+
+    const resultWrappedSegwit = xprivToXPub({
+      xpriv:
+        'yprvAHwhK6RbpuS3dgCYHM5jc2ZvEKd7Bi61u9FVhYMpgMSuZS613T1xxQeKTffhrHY79hZ5PsskBjcc6C2V7DrnsMsNaGDaWev3GLRQRgV7hxF',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit
+    })
+    expect(resultWrappedSegwit).to.equals(
+      'ypub6Ww3ibxVfGzLrAH1PNcjyAWenMTbbAosGNB6VvmSEgytSER9azLDWCxoJwW7Ke7icmizBMXrzBx9979FfaHxHcrArf3zbeJJJUZPf663zsP'
+    )
+
+    const resultWrappedSegwitTestnet = xprivToXPub({
+      xpriv:
+        'uprv91G7gZkzehuMVxDJTYE6tLivdF8e4rvzSu1LFfKw3b2Qx1Aj8vpoFnHdfUZ3hmi9jsvPifmZ24RTN2KhwB8BfMLTVqaBReibyaFFcTP1s9n',
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit
+    })
+    expect(resultWrappedSegwitTestnet).to.equals(
+      'upub5EFU65HtV5TeiSHmZZm7FUffBGy8UKeqp7vw43jYbvZPpoVsgU93oac7Wk3u6moKegAEWtGNF8DehrnHtv21XXEMYRUocHqguyjknFHYfgY'
+    )
+
+    const resultLegacy = xprivToXPub({
+      xpriv:
+        'xprv9xpXFhFpqdQK3TmytPBqXtGSwS3DLjojFhTGht8gwAAii8py5X6pxeBnQ6ehJiyJ6nDjWGJfZ95WxByFXVkDxHXrqu53WCRGypk2ttuqncb',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Legacy
+    })
+    expect(resultLegacy).to.equals(
+      'xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj'
+    )
+
+    const resultLegacyTestnet = xprivToXPub({
+      xpriv:
+        'tprv8fVU32aAEuEPeH1WYx3LhXtSFZTRaFqjbFNPaJZ9R8fCVja44tSaUPZEKGpMK6McUDkWWMvRiVfKR3Wzei6AmLoTNYHMAZ9KtvVTLZZdhvA',
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Legacy
+    })
+    expect(resultLegacyTestnet).to.equals(
+      'tpubDCBWBScQPGv4Xk3JSbhw6wYYpayMjb2eAYyArpbSqQTbLDpphHGAetB6VQgVeftLML8vDSUEWcC2xDi3qJJ3YCDChJDvqVzpgoYSuT52MhJ'
+    )
+  })
+})
+
+describe('xpub to address tests', () => {
+  it('given different xpubs, generate a valid address by calling xpubToScriptHash and scriptHashToAddress', () => {
+    /*
+    These methods were cross verified using ian colemans bip32 website https://iancoleman.io/bip39/
+    using the same seed as in other tests (abandon, ...)
+    */
+
+    const scriptHashP2PKH = xpubToScriptHash({
+      xpub:
+        'xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Legacy,
+      addressType: AddressTypeEnum.p2pkh,
+      bip44ChangeIndex: 0,
+      bip44AddressIndex: 0
+    })
+    const p2pkhAddress = scriptHashToAddress({
+      scriptHash: scriptHashP2PKH,
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2pkh
+    })
+    expect(p2pkhAddress).to.equals('1LqBGSKuX5yYUonjxT5qGfpUsXKYYWeabA')
+    const scriptHashP2PKHRoundTrip = addressToScriptHash({
+      address: '1LqBGSKuX5yYUonjxT5qGfpUsXKYYWeabA',
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2pkh
+    })
+    // TODO: Drop this string comparison and use equals bytes instead
+    expect(scriptHashP2PKHRoundTrip?.toString()).to.equals(
+      scriptHashP2PKH?.toString()
+    )
+
+    const scriptHashP2SH = xpubToScriptHash({
+      xpub:
+        'xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Legacy,
+      addressType: AddressTypeEnum.p2sh,
+      bip44ChangeIndex: 0,
+      bip44AddressIndex: 0
+    })
+    const p2shAddress = scriptHashToAddress({
+      scriptHash: scriptHashP2SH,
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2sh
+    })
+    expect(p2shAddress).to.equals('3Hggo5JYqytA5k5As5XuADi6TYGQmMZrND')
+    const scriptHashP2SHRoundTrip = addressToScriptHash({
+      address: '3Hggo5JYqytA5k5As5XuADi6TYGQmMZrND',
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2sh
+    })
+    expect(scriptHashP2SHRoundTrip?.toString()).to.equals(
+      scriptHashP2SH?.toString()
+    )
+
+    const scriptHashP2WPKHP2SH = xpubToScriptHash({
+      xpub:
+        'ypub6Ww3ibxVfGzLrAH1PNcjyAWenMTbbAosGNB6VvmSEgytSER9azLDWCxoJwW7Ke7icmizBMXrzBx9979FfaHxHcrArf3zbeJJJUZPf663zsP',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit,
+      addressType: AddressTypeEnum.p2wpkhp2sh,
+      bip44ChangeIndex: 0,
+      bip44AddressIndex: 0
+    })
+    const p2wpkhp2shAddress = scriptHashToAddress({
+      scriptHash: scriptHashP2WPKHP2SH,
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2wpkhp2sh
+    })
+    expect(p2wpkhp2shAddress).to.equals('37VucYSaXLCAsxYyAPfbSi9eh4iEcbShgf')
+    const scriptHashP2WPKHP2SHRoundTrip = addressToScriptHash({
+      address: '37VucYSaXLCAsxYyAPfbSi9eh4iEcbShgf',
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2wpkhp2sh
+    })
+    expect(scriptHashP2WPKHP2SHRoundTrip?.toString()).to.equals(
+      scriptHashP2WPKHP2SH?.toString()
+    )
+
+    const scriptHashP2WPKH = xpubToScriptHash({
+      xpub:
+        'zpub6rFR7y4Q2AijBEqTUquhVz398htDFrtymD9xYYfG1m4wAcvPhXNfE3EfH1r1ADqtfSdVCToUG868RvUUkgDKf31mGDtKsAYz2oz2AGutZYs',
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Segwit,
+      addressType: AddressTypeEnum.p2wpkh,
+      bip44ChangeIndex: 0,
+      bip44AddressIndex: 0
+    })
+    const p2wpkhAddress = scriptHashToAddress({
+      scriptHash: scriptHashP2WPKH,
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2wpkh
+    })
+    expect(p2wpkhAddress).to.equals(
+      'bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu'
+    )
+    const scriptHashP2WPKHRoundTrip = addressToScriptHash({
+      address: 'bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu',
+      network: NetworkEnum.Mainnet,
+      addressType: AddressTypeEnum.p2wpkh
+    })
+    expect(scriptHashP2WPKHRoundTrip?.toString()).to.be.equal(
+      scriptHashP2WPKH?.toString()
+    )
+  })
+})
+
+describe('transaction creation and signing test', () => {
+  // key with control on the unspent output and used to sign the transaction
+  const wifKey = 'L2uPYXe17xSTqbCjZvL2DsyXPCbXspvcu5mHLDYUgzdUbZGSKrSr'
+  const ecKeyPair = wifToECPair({ wifKey })
+  const scriptPubkey = pubkeyToScriptPubkey({
+    pubkey: ecKeyPair.publicKey,
+    addressType: AddressTypeEnum.p2pkh
+  })
+  it('Create transaction with one legacy input and one output', () => {
+    const hexTx: string = createTx({
+      network: NetworkEnum.Mainnet,
+      inputs: [
+        {
+          type: TransactionInputTypeEnum.Legacy,
+          prev_txid:
+            '7d067b4a697a09d2c3cff7d4d9506c9955e93bff41bf82d439da7d030382bc3e',
+          // prev_tx only for non segwit inputs
+          prev_txout: Buffer.from(
+            '0200000001f9f34e95b9d5c8abcd20fc5bd4a825d1517be62f0f775e5f36da944d9' +
+              '452e550000000006b483045022100c86e9a111afc90f64b4904bd609e9eaed80d48' +
+              'ca17c162b1aca0a788ac3526f002207bb79b60d4fc6526329bf18a77135dc566020' +
+              '9e761da46e1c2f1152ec013215801210211755115eabf846720f5cb18f248666fec' +
+              '631e5e1e66009ce3710ceea5b1ad13ffffffff01' +
+              // value in satoshis (Int64LE) = 0x015f90 = 90000
+              '905f010000000000' +
+              // scriptPubkey length
+              '19' +
+              // scriptPubkey
+              scriptPubkey.toString('hex') +
+              // locktime
+              '00000000',
+            'hex'
+          ),
+          index: 0,
+          // prev_scriptPubkey only relevant for Segwit inputs, but keep mandator for now before we start handling errors.
+          prev_scriptPubkey: scriptPubkey,
+          sequence: 0xffffffff
+        }
+      ],
+      outputs: [
+        {
+          scriptPubkey: addressToScriptPubkey({
+            address: '1KRMKfeZcmosxALVYESdPNez1AP1mEtywp',
+            network: NetworkEnum.Mainnet,
+            addressType: AddressTypeEnum.p2pkh
+          }),
+          amount: 8000
+        }
+      ],
+      locktime: 0,
+      privateKey: ecKeyPair
+    })
+    expect(hexTx).to.equal(
+      '02000000013ebc8203037dda39d482bf41ff3be955996c50d9d4f7cfc3d2097a694a7' +
+        'b067d000000006b483045022100931b6db94aed25d5486884d83fc37160f37f3368c0' +
+        'd7f48c757112abefec983802205fda64cff98c849577026eb2ce916a50ea70626a766' +
+        '9f8596dd89b720a26b4d501210365db9da3f8a260078a7e8f8b708a1161468fb2323f' +
+        'fda5ec16b261ec1056f455ffffffff0180380100000000001976a914ca0d36044e0dc' +
+        '08a22724efa6f6a07b0ec4c79aa88ac00000000'
+    )
+  })
+})
+
 describe('edge-rest-wallet', function() {
   it("Doesn't have tests yet", function() {
     expect(1 + 1).equals(2)
