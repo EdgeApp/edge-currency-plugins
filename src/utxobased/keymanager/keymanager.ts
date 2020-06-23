8adf262fb3dfb781b857d7836b77d00d9219f826
TheCharlatan <seb.kung@gmail.com>
2020-06-23 10:48:30 +0200

    Move code to its own utxobase/keymanager subdirectory

--- src/utxobased/keymanager/keymanager.ts
+++ src/utxobased/keymanager/keymanager.ts
@@ -206,6 +206,37 @@ export class Bitcoin implements Coin {
   }
 }
 
+export class Litecoin implements Coin {
+  name: string = 'litecoin'
+  mainnetConstants = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    wif: 0xb0,
+    legacyXPriv: 0x019d9cfe,
+    legacyXPub: 0x019da462,
+    wrappedSegwitXPriv: 0x01b26792,
+    wrappedSegwitXPub: 0x01b26ef6,
+    segwitXPriv: 0x04b2430c,
+    segwitXPub: 0x04b24746,
+    pubkeyHash: 0x30,
+    scriptHash: 0x32,
+    bech32: 'ltc'
+  }
+
+  testnetConstants = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    wif: 0xef,
+    legacyXPriv: 0x04358394,
+    legacyXPub: 0x043587cf,
+    wrappedSegwitXPriv: 0x044a4e28,
+    wrappedSegwitXPub: 0x044a5262,
+    segwitXPriv: 0x045f18bc,
+    segwitXPub: 0x045f1cf6,
+    pubkeyHash: 0x6f,
+    scriptHash: 0x3a,
+    bech32: 'tltc'
+  }
+}
+
 // BitcoinJSNetwork and Bip32 are the same interfaces as declared in  bitcoin-js ts_src/network.ts
 // We redeclare them here for transparency reasons
 interface BitcoinJSNetwork {
--- test/dummy-test.ts
+++ test/dummy-test.ts
@@ -8,6 +8,7 @@ import {
   BIP43PurposeTypeEnum,
   Bitcoin,
   createTx,
+  Litecoin,
   mnemonicToXPriv,
   NetworkEnum,
   privateKeyHexStrToWIF,
@@ -23,9 +24,9 @@ import {
   wifToPrivateKeyHexStr,
   xprivToXPub,
   xpubToScriptHash
-} from '../src/index'
+} from '../src/utxobased/keymanager/index'
 
-describe('mnemonic to xprv test vectors as collected from BIP84, BIP49 and some generated cases to test xpub prefix bytes', () => {
+describe('bitcoin mnemonic to xprv test vectors as collected from BIP84, BIP49 and some generated cases to test xpub prefix bytes', () => {
   const mnemonic =
     'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
   it('bip84 mnemonic to xpriv mainnet', () => {
@@ -96,13 +97,95 @@ describe('mnemonic to xprv test vectors as collected from BIP84, BIP49 and some 
   it('bip44 mnemonic to xpriv testnet', () => {
     const resultLegacyTestnet = mnemonicToXPriv({
       mnemonic: mnemonic,
-      path: "m/44'/0'/0'",
+      path: "m/44'/1'/0'",
       network: NetworkEnum.Testnet,
       type: BIP43PurposeTypeEnum.Legacy,
       coin: new Bitcoin()
     })
     expect(resultLegacyTestnet).to.equal(
-      'tprv8fVU32aAEuEPeH1WYx3LhXtSFZTRaFqjbFNPaJZ9R8fCVja44tSaUPZEKGpMK6McUDkWWMvRiVfKR3Wzei6AmLoTNYHMAZ9KtvVTLZZdhvA'
+      'tprv8fPDJN9UQqg6pFsQsrVxTwHZmXLvHpfGGcsCA9rtnatUgVtBKxhtFeqiyaYKSWydunKpjhvgJf6PwTwgirwuCbFq8YKgpQiaVJf3JCrNmkR'
+    )
+  })
+})
+
+describe('litecoin mnemonic to xprv test vectors as collected from BIP84, BIP49 and some generated cases to test xpub prefix bytes', () => {
+  const mnemonic =
+    'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
+  it('bip84 mnemonic to xpriv mainnet', () => {
+    const resultSegwit = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/84'/2'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Segwit,
+      coin: new Litecoin()
+    })
+    expect(resultSegwit).to.equal(
+      'zprvAdQSgFiAHcXKb1gcktyukXyGZykTBemmPZ9brfYnxqxM2CocMdxy9aPXTbTLv7dvJgWn2Efi4vFSyPbT4QqgarYrs583WCeMXM2q3TUU8FS'
+    )
+  })
+
+  it('bip84 mnemonic to xpriv testnet', () => {
+    const resultSegwitTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/84'/1'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Segwit,
+      coin: new Litecoin()
+    })
+    expect(resultSegwitTestnet).to.equal(
+      'vprv9K7GLAaERuM58PVvbk1sMo7wzVCoPwzZpVXLRBmum93gL5pSqQCAAvZjtmz93nnnYMr9i2FwG2fqrwYLRgJmDDwFjGiamGsbRMJ5Y6siJ8H'
+    )
+  })
+
+  it('bip49 mnemonic to xpriv mainnet', () => {
+    const resultWrappedSegwit = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/49'/2'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit,
+      coin: new Litecoin()
+    })
+    expect(resultWrappedSegwit).to.equal(
+      'Mtpv7RooeEQDUitupgpJcxZnfDwvq8hC24R7GAiscrqFhHHhit96vCNY7yudJgrM841dMbiRUQceC12566XAHHC8Rd1BtnBdokq9tmF7jLLvUdh'
+    )
+  })
+
+  it('bip49 mnemonic to xpriv testnet', () => {
+    const resultWrappedSegwitTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/49'/1'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.WrappedSegwit,
+      coin: new Litecoin()
+    })
+    expect(resultWrappedSegwitTestnet).to.equal(
+      'uprv91G7gZkzehuMVxDJTYE6tLivdF8e4rvzSu1LFfKw3b2Qx1Aj8vpoFnHdfUZ3hmi9jsvPifmZ24RTN2KhwB8BfMLTVqaBReibyaFFcTP1s9n'
+    )
+  })
+
+  it('bip44 mnemonic to xpriv mainnet', () => {
+    const resultLegacy = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/44'/2'/0'",
+      network: NetworkEnum.Mainnet,
+      type: BIP43PurposeTypeEnum.Legacy,
+      coin: new Litecoin()
+    })
+    expect(resultLegacy).to.equal(
+      'Ltpv7735AbcbmL1gbgDWj2ezvs59rh4RM1oTN2BKTKbfe3146FCPCNFbBBSWfuV9vCJNMXD9LuHpQnqVWpn2hbMhikqPdoGqbS3ptdPoNWEvvgR'
+    )
+  })
+
+  it('bip44 mnemonic to xpriv testnet', () => {
+    const resultLegacyTestnet = mnemonicToXPriv({
+      mnemonic: mnemonic,
+      path: "m/44'/1'/0'",
+      network: NetworkEnum.Testnet,
+      type: BIP43PurposeTypeEnum.Legacy,
+      coin: new Litecoin()
+    })
+    expect(resultLegacyTestnet).to.equal(
+      'tprv8fPDJN9UQqg6pFsQsrVxTwHZmXLvHpfGGcsCA9rtnatUgVtBKxhtFeqiyaYKSWydunKpjhvgJf6PwTwgirwuCbFq8YKgpQiaVJf3JCrNmkR'
     )
   })
 })
