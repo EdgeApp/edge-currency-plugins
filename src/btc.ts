904f5587a94650cf547332840f8003863c25c6b6
Noah Passalacqua <noah@passabilities.dev>
2020-11-12 15:30:15 -0600

    Fix btc plugin export.

--- src/btc.ts
+++ src/btc.ts
@@ -3,7 +3,11 @@ import { EdgeCorePluginOptions, EdgeCorePlugins } from 'edge-core-js'
 import { makeCurrencyPlugin } from './common/plugin/makeCurrencyPlugin'
 import { info } from './common/utxobased/info/bitcoin'
 
-export default {
+const plugin = {
   [info.pluginId]: (options: EdgeCorePluginOptions) =>
-    makeCurrencyPlugin(options, info)
+    makeCurrencyPlugin(options, info),
 } as EdgeCorePlugins
+
+window?.addEdgeCorePlugins?.(plugin)
+
+export default plugin
