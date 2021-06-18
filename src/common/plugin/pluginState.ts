9a0c837cb28fdd42d68b1da749c9bb72b0fae475
TheCharlatan <seb.kung@gmail.com>
2021-06-18 17:04:07 +0200

    Plugin state: Use memlet for persistence

--- src/common/plugin/pluginState.ts
+++ src/common/plugin/pluginState.ts
@@ -1,7 +1,8 @@
 // Typescript translation from original code in edge-currency-bitcoin
 
-import { Disklet, navigateDisklet } from 'disklet'
+import { navigateDisklet } from 'disklet'
 import { EdgeIo, EdgeLog } from 'edge-core-js'
+import { makeMemlet, Memlet } from 'memlet'
 
 import { UtxoEngineState } from '../utxobased/engine/makeUtxoEngineState'
 import { ServerCache, ServerInfo } from './serverCache'
@@ -76,7 +77,7 @@ export class PluginState extends ServerCache {
   infoServerUris: string
 
   engines: UtxoEngineState[]
-  disklet: Disklet
+  memlet: Memlet
 
   serverCacheJson: { [serverUrl: string]: ServerInfo }
   pluginId: string
@@ -100,7 +101,7 @@ export class PluginState extends ServerCache {
       InfoServer
     )}/blockBookServers/${JSON.stringify(fixedCode)}`
     this.engines = []
-    this.disklet = navigateDisklet(io.disklet, 'plugins/' + pluginId)
+    this.memlet = makeMemlet(navigateDisklet(io.disklet, 'plugins/' + pluginId))
 
     this.pluginId = pluginId
     this.serverCacheJson = {}
@@ -108,11 +109,7 @@ export class PluginState extends ServerCache {
 
   async load(): Promise<PluginState> {
     try {
-      const serverCacheText = await this.disklet.getText('serverCache.json')
-      const serverCacheJson = JSON.parse(serverCacheText)
-      // TODO: Validate JSON
-
-      this.serverCacheJson = serverCacheJson
+      this.serverCacheJson = await this.memlet.getJson('serverCache.json')
     } catch (e) {
       this.log(
         `${this.pluginId}: Failed to load server cache: ${JSON.stringify(e)}`
@@ -138,10 +135,7 @@ export class PluginState extends ServerCache {
     // this.printServerCache()
     if (this.serverCacheDirty) {
       try {
-        await this.disklet.setText(
-          'serverCache.json',
-          JSON.stringify(this.servers_)
-        )
+        await this.memlet.setJson('serverCache.json', this.servers_)
         this.serverCacheDirty = false
         this.cacheLastSave_ = Date.now()
         this.log(`${this.pluginId} - Saved server cache`)
