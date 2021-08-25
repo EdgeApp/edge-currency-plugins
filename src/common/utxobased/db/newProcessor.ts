f56d44ec2bfca61658c662677e88c3e251087306
TheCharlatan <seb.kung@gmail.com>
2021-08-25 13:58:03 +0200

    NewProcessor: Add new baselets / deprecate old baselets

--- src/common/utxobased/db/newProcessor.ts
+++ src/common/utxobased/db/newProcessor.ts
@@ -12,14 +12,9 @@ interface ProcessorConfig {
 
 /* Transaction table interfaces */
 
-interface InsertTransactionArgs {
+interface SaveTransactionArgs {
   tx: IProcessorTransaction
-  scriptPubkey: string
-}
-
-interface UpdateTransactionArgs {
-  txId: string
-  data: Pick<IProcessorTransaction, 'blockHeight'>
+  scriptPubkey?: string
 }
 
 interface FetchTransactionArgs {
@@ -30,11 +25,6 @@ interface FetchTransactionArgs {
 }
 
 /* Address table interfaces */
-
-interface UpdateAddressArgs {
-  scriptPubkey: string
-  data: Partial<IAddress>
-}
 
 interface FetchAddressArgs {
   path?: Omit<AddressPath, 'addressIndex'> | AddressPath
@@ -61,12 +51,10 @@ export interface NewProcessor {
   Used to store UTXOs. Needs to be able to track UTXOs that are already used in
   a transaction, but not confirmed yet */
 
-  insertUtxo: (utxo: IUTXO) => Promise<void>
+  saveUtxo: (utxo: IUTXO) => Promise<void>
   // remove either all UTXOs if the array is empty, or as selected from an array
   // of UTXO ids
   removeUtxos: (utxoIds: string[]) => Promise<void>
-  // replaces a utxo with the same id, for example to set the 'used' flag
-  updateUtxo: (utxo: IUTXO) => Promise<void>
   // fetch either all UTXOs if the array is empty or as selected from an array
   // of UTXO ids
   fetchUtxos: (utxoIds: string[]) => Promise<IUTXO[]>
@@ -80,17 +68,15 @@ export interface NewProcessor {
   and discover used addresses
   txIdsByBlockHeight: index from block height to txid, used to change
   confirmation height for unconfirmed transactions
-  txsByDate: index from date to txid, used for EdgeGetTransactionsOptions
+  txIdsByDate: index from date to txid, used for EdgeGetTransactionsOptions
   querying
   ------------------------------- 
   Used to store transactions. Needs to be updated for confirmation
   heights, and detecting used script pubkeys */
 
-  insertTransaction: (args: InsertTransactionArgs) => Promise<void>
+  saveTransaction: (args: SaveTransactionArgs) => Promise<void>
   numTransactions: () => number
   removeTransaction: (txId: string) => Promise<void>
-  // update confirmation height
-  updateTransaction: (args: UpdateTransactionArgs) => Promise<void>
   fetchTransactions: (
     args: FetchTransactionArgs
   ) => Promise<IProcessorTransaction[]>
@@ -101,20 +87,18 @@ export interface NewProcessor {
   ===============================
   addressByScriptPubkey: main table
   scriptPubkeyByPath: index from path to script pubkey
-  lastUsedByFormatPath: index from path to last used derivation index
+  lastUsedByFormatPath: index from path to last used address derivation index
   ------------------------------- 
   Used to store script pubkeys / addresses. Needs to be updated for 'used' flag
   and path */
 
-  insertAddress: (args: IAddress) => Promise<void>
+  saveAddress: (args: IAddress) => Promise<void>
   // used to calculate total number of addresses
   numAddressesByFormatPath: (path: Omit<AddressPath, 'addressIndex'>) => number
-  // get the last used address for a specific format
+  // get the last used address index for a specific format
   lastUsedScriptPubkeyByFormatPath: (
     path: Omit<AddressPath, 'addressIndex'>
   ) => Promise<string>
-  // update to set the path or the used flag
-  updateAddress: (args: UpdateAddressArgs) => Promise<void>
   fetchAddresses: (args: FetchAddressArgs) => Promise<IAddress[]>
 
   /* Block processing
@@ -128,7 +112,7 @@ export interface NewProcessor {
 
   // insert a new block height / block hash pair. Evicts pairs further back in
   // history than the threshold blocks
-  insertBlockHash: (args: BlockHeightArgs) => Promise<void>
+  saveBlockHash: (args: BlockHeightArgs) => Promise<void>
   fetchBlockHash: (height: number) => Promise<string[]>
 }
 
@@ -140,7 +124,7 @@ export async function makeNewProcessor(
       //
     },
 
-    async insertUtxo(_utxo: IUTXO): Promise<void> {
+    async saveUtxo(_utxo: IUTXO): Promise<void> {
       return
     },
 
@@ -148,15 +132,11 @@ export async function makeNewProcessor(
       return
     },
 
-    async updateUtxo(_utxo: IUTXO): Promise<void> {
-      return
-    },
-
     async fetchUtxos(_utxoIds: string[]): Promise<IUTXO[]> {
       return []
     },
 
-    async insertTransaction(_args: InsertTransactionArgs): Promise<void> {
+    async saveTransaction(_args: SaveTransactionArgs): Promise<void> {
       return
     },
 
@@ -168,17 +148,13 @@ export async function makeNewProcessor(
       return
     },
 
-    async updateTransaction(_args: UpdateTransactionArgs): Promise<void> {
-      return
-    },
-
     async fetchTransactions(
       _args: FetchTransactionArgs
     ): Promise<IProcessorTransaction[]> {
       return []
     },
 
-    async insertAddress(_args: IAddress): Promise<void> {
+    async saveAddress(_args: IAddress): Promise<void> {
       return
     },
 
@@ -192,15 +168,11 @@ export async function makeNewProcessor(
       return ''
     },
 
-    async updateAddress(_args: UpdateAddressArgs): Promise<void> {
-      return
-    },
-
     async fetchAddresses(_args: FetchAddressArgs): Promise<IAddress[]> {
       return []
     },
 
-    async insertBlockHash(_args: BlockHeightArgs): Promise<void> {
+    async saveBlockHash(_args: BlockHeightArgs): Promise<void> {
       return
     },
 
