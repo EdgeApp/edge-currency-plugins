c7202477703fa3320351eb1e6e357ba92327413d
Samuel Holmes <sam@edge.app>
2021-12-29 20:27:25 -0800

    Add TODO comment to left-over code for updating UTXO spent property

--- src/common/utxobased/engine/makeUtxoEngineState.ts
+++ src/common/utxobased/engine/makeUtxoEngineState.ts
@@ -370,6 +370,14 @@ export function makeUtxoEngineState(
     },
 
     async broadcastTx(transaction: EdgeTransaction): Promise<string> {
+      /*
+      The code below appears to be left-over implementation for RBF, however the
+      UTXO `spent` property has no effect within the current implementation and
+      the UTXOs are eventually removed once the transaction is broadcast and the
+      engine syncs with the network.
+
+      TODO: Figure out whether this code is still needed.
+      */
       // put spent utxos into an interim data structure (saveSpentUtxo)
       // these utxos are removed once the transaction confirms
       const [tx] = await processor.fetchTransactions({ txId: transaction.txid })
